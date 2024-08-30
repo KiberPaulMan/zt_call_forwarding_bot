f9839c2d67bfe10b63cb7fe67133854c6f0b97d2
PaulMan <32840889+KiberPaulMan@users.noreply.github.com>
2024-08-30 14:23:12 +0700

    delete inline button

--- keyboards/reply.py
+++ keyboards/reply.py
@@ -17,12 +17,13 @@ inline_kb1 = InlineKeyboardMarkup(
 person_btn_1 = InlineKeyboardButton(text=list(phonebook.values())[0][1], callback_data=list(phonebook.keys())[0])
 person_btn_2 = InlineKeyboardButton(text=list(phonebook.values())[1][1], callback_data=list(phonebook.keys())[1])
 person_btn_3 = InlineKeyboardButton(text=list(phonebook.values())[2][1], callback_data=list(phonebook.keys())[2])
-person_btn_4 = InlineKeyboardButton(text=list(phonebook.values())[3][1], callback_data=list(phonebook.keys())[3])
+# person_btn_4 = InlineKeyboardButton(text=list(phonebook.values())[3][1], callback_data=list(phonebook.keys())[3])
 
 person_kb1 = InlineKeyboardMarkup(
     inline_keyboard=[
-        [person_btn_1, person_btn_2],
-        [person_btn_3, person_btn_4],
+        [person_btn_1],
+        [person_btn_2],
+        [person_btn_3],
     ]
 )
 
