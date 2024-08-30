2d2c9831984f5723ee5f8c03cf01ae90267c823a
PaulMan <32840889+KiberPaulMan@users.noreply.github.com>
2024-08-30 14:45:33 +0700

    fix out index

--- parse/duty_file_analysis.py
+++ parse/duty_file_analysis.py
@@ -51,12 +51,12 @@ def parse_excel_data(excel_data):
         #         data = data if isinstance(data, int) else 0
         #         parse_data[list(phonebook.keys())[3]].append(data)
 
-        if out_index == 7:  # Кохан Антон
+        if out_index == 8:  # Кохан Антон
             for data in out_data[1][2:33]:
                 data = data if isinstance(data, int) else 0
                 parse_data[list(phonebook.keys())[2]].append(data)
 
-        if out_index == 8:  # Эрднев Влад
+        if out_index == 9:  # Эрднев Влад
             for data in out_data[1][2:33]:
                 data = data if isinstance(data, int) else 0
                 parse_data[list(phonebook.keys())[1]].append(data)
